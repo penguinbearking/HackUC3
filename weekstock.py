1f7150725c2f396927cb416f275f580998daa656
penguinbearking <43753068+penguinbearking@users.noreply.github.com>
2018-10-13 16:27:08 -0400

    added ui choices

--- weekstock.py
+++ weekstock.py
@@ -6,38 +6,51 @@ import pandas
 import json
 
 
-stock = input("Enter your ticker symbol: " )
+while True:
+    userquit = input("Type quit to exit the program")
+    if userquit.lower() == ("quit"):
+        break
+    
+    stock = input("Enter your ticker symbol: " )
 
 
-today = date.today()
-lastweek = date.today() - timedelta(8)
+    today = date.today()
+    lastweek = date.today() - timedelta(7)
 
-print(lastweek)
-print(today)
+    print(lastweek)
+    print(today)
 
-start = lastweek
+    lastmonth = date.today() - timedelta(30)
+    lastyear = date.today() - timedelta(365)
 
-end = today
+    period = input("Enter time period: (week, month, and year)")
+    if (period == "week"):
+      start = lastweek
+    if (period == "month"):
+      start = lastmonth
+    if (period == "year"):
+      start = lastyear
+    end = today
 
-data = get_historical_data(stock, start, end, output_format='pandas')
-a = type(data)
-print(a)
+    data = get_historical_data(stock, start, end, output_format='pandas')
+    a = type(data)
+    print(a)
 
-datalist = data.values.tolist()
-print(datalist)
+    datalist = data.values.tolist()
+    print(datalist)
 
-"""
-i = 0
-for i in range(len(data)):
-    j = (date.today() - timedelta(i)).weekday()
-    if j == 5 or j == 6:
-        data.pop(i)
-        print ("weekend")
-        i +=1
+    i = 0
+    for i in range(len(datalist[4])):
+        j = (date.today() - timedelta(i)).weekday()
+        if j == 5 or j == 6:
+            datalist[4].pop(i)
+            print ("weekend")
+            i +=1
 
-plt.plot(data["close"])
+    plt.plot(data["close"])
 
-plt.title('Time series chart for ' + (stock))
+    plt.title('Time series chart for ' + (stock))
 
-plt.show()
-"""
+    plt.show()
+
+exit()
